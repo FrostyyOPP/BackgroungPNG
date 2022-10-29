667c0e76030d7eac181efbd874b3e3d5436001e5
Ranjodh Singh <workwithranjodh@gmail.com>
2022-10-29 09:16:56 +0530

    front end allmost done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,17 +7,17 @@ export const Header = () => {
 
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/home">
+                <a className="navbar-brand" href="/">
                     <img src="https://cryptologos.cc/logos/aave-aave-logo.png" alt="Bootstrap" width="50" height="44" />
                 </a>
-                <a className="navbar-brand" href="/home">Background PNG</a>
+                <a className="navbar-brand" href="/">Background PNG</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" href="/home">Home</a>
+                            <a className="nav-link active" href="/">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/wallpapers">Wallpapers</a>
